86e4c83b5500dcb4b8f8e26edceb2e2558a93a72
kousuke takahashi <pamarikomi_t@icloud.com>
2022-03-09 22:10:29 +0900

    created jucSample

---
+++ TwoPhaseTermination/jucSample1/Main.java
@@ -0,0 +1,24 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.CountDownLatch;
+
+public class Main {
+  private static final int TASKS = 10;
+
+  public static void main(String[] args) {
+    System.out.println("BEGIN");
+    ExecutorService service = Executors.newFixedThreadPool(5);
+    CountDownLatch doneLatch = new CountDownLatch(TASKS);
+    try {
+      for(int t = 0; t < TASKS; t++) {
+        service.execute(new MyTask(doneLatch, t));
+      }
+      System.out.println("AWAIT");
+      doneLatch.await();
+    } catch (InterruptedException e) {
+    } finally {
+      service.shutdown();
+      System.out.println("END");
+    }
+  }
+}
