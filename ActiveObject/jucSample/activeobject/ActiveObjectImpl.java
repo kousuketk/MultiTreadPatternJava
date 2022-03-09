c3ed4ef82ca73ff4a93c68d472977996dbfc611d
kousuke takahashi <pamarikomi_t@icloud.com>
2022-03-10 01:23:15 +0900

    crated jucSample

---
+++ ActiveObject/jucSample/activeobject/ActiveObjectImpl.java
@@ -0,0 +1,45 @@
+package activeobject;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+
+class ActiveObjectImpl implements ActiveObject {
+  private final ExecutorService service = Executors.newSingleThreadExecutor();
+  
+  public void shutdown() {
+    service.shutdown();
+  }
+
+  public Future<String> makeString(final int count, final char fillchar) {
+    class MakeStringRequest implements Callable<String> {
+      public String call() {
+        char[] buffer = new char[count];
+        for (int i = 0; i < count; i++) {
+          buffer[i] = fillchar;
+          try {
+            Thread.sleep(100);
+          } catch (InterruptedException e) {
+          }
+        }
+        return new String(buffer);
+      }
+    }
+    return service.submit(new MakeStringRequest());
+  }
+
+  public void displayString(final String string) {
+    class DisplayStringRequest implements Runnable {
+      public void run() {
+        try {
+          System.out.println("displayString: " + string);
+          Thread.sleep(10);
+        } catch (InterruptedException e) {
+        }
+      }
+    }
+    // リクエストの発行
+    service.execute(new DisplayStringRequest());
+  }
+}
